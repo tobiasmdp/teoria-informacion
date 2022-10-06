eaaedc742d052e2fc5da84df7bbe35ff39f30af8
mikel <mikelalejandroolave@gmail.com>
2022-10-06 18:28:45 -0300

    se agrego vector estacionario

--- ejercicio-1-1-a/main.c
+++ ejercicio-1-1-a/main.c
@@ -8,7 +8,10 @@ void IngresoAbecedario(char [],int*);
 void LeeArch(char Vsimbolos[],int ,int  [][MAX],int  []);
 void MostrarVector(int cantidad, int VContsimbolos [] );
 void MostrarMatriz(int cantidad, int MContSimbolos [] [MAX]);
+void MostrarMatrizFloat(int cantidad, float Matriz[] [MAX]);
 void Resultados(int MContSimbolos [] [MAX],int cantidad, int VContsimbolos [] );
+void VectorEstacionario(int MContSimbolos [] [MAX],int cantidad,int VContsimbolos[]);
+int checkTolerancia(float MatrizNueva[][MAX],float MatrizActual[][MAX],int, float );
 
 int main()
 {
@@ -22,9 +25,9 @@ int main()
     }*/
     LeeArch(Vsimbolos,cantidad,MContSimbolos,VContsimbolos);
     Resultados(MContSimbolos,cantidad,VContsimbolos);
+    VectorEstacionario(MContSimbolos,cantidad,VContsimbolos);
     return 0;
 }
-
 
 void LeeArch(char Vsimbolos[],int cantidad,int MContSimbolos [][MAX],int VContsimbolos []){
     int j,pos,posant=-1;
@@ -72,6 +75,18 @@ void MostrarMatriz(int cantidad, int MContSimbolos [] [MAX]){
 
 }
 
+void MostrarMatrizFloat(int cantidad, float Matriz [] [MAX]){
+    int i,j;
+
+    for(i=0;i<cantidad;i++){
+        for(j=0;j<cantidad;j++){
+        printf("%f  ",Matriz[i][j]);
+        }
+        printf("\n");
+    }
+
+}
+
 void IngresoAbecedario(char Vsimbolos[],int *cantidad){
 
     int i;
@@ -86,9 +101,52 @@ void IngresoAbecedario(char Vsimbolos[],int *cantidad){
 }
 
 void Resultados(int MContSimbolos [] [MAX],int cantidad, int VContsimbolos [] ){
+    printf("matriz resultado: \n");
     for (int i=0; i<cantidad;i++){
         for (int j=0;j<cantidad;j++)
             printf("%2.4f ",(float)MContSimbolos[i][j]/VContsimbolos[j]);
         printf("\n");
     }
+}
+
+void VectorEstacionario(int MContSimbolos [] [MAX],int cantidad,int VContsimbolos[]){
+float suma,MatrizAnterior[MAX][MAX],MatrizActual[MAX][MAX],MatrizNueva[MAX][MAX];
+int a=0,c,d,k,i,j;
+int vecresultado[MAX];
+float tolerancia=0.00001;
+
+    for (i=0;i<cantidad;i++)
+        for (j=0;j<cantidad;j++){
+            MatrizActual[i][j]=(float)MContSimbolos[i][j]/VContsimbolos[j];
+            MatrizNueva[i][j]=(float)MContSimbolos[i][j]/VContsimbolos[j];
+        }
+    do{
+        for (i=0;i<cantidad;i++)
+            for (j=0;j<cantidad;j++){
+                MatrizAnterior[i][j]=MatrizActual[i][j];
+                MatrizActual[i][j]=MatrizNueva[i][j];
+            }
+        for (c = 0; c < cantidad; c++) 
+            for (d = 0; d < cantidad; d++){
+                for (k = 0; k < cantidad; k++) 
+                    suma += MatrizAnterior[c][k] * MatrizActual[k][d];
+                MatrizNueva[c][d] = suma;
+                suma = 0;
+            }
+        a++;
+        printf("Iteracion numero = %d \n",a);
+        printf("Matriz de la iteracion: \n");
+        MostrarMatrizFloat(cantidad,MatrizNueva);
+    }
+    while (checkTolerancia(MatrizNueva,MatrizActual,cantidad,tolerancia));
+}
+
+int checkTolerancia(float MatrizNueva[][MAX],float MatrizActual[][MAX],int cantidad, float tolerancia){
+int i=0,j=0;
+    for (i=0;i<cantidad;i++)
+        for (j=0;j<cantidad;j++){
+           if (tolerancia<MatrizActual[i][j]-MatrizNueva[i][j])
+            return 1;
+    }
+    return 0;
 }
